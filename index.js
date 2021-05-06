e92ba0228cede34252d2102209e73285646bcaed
samzaberi <samz.aberi@gmail.com>
2021-05-06 14:37:22 +0300

    ex3.8 code

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 
 app.use(express.json())
 
-app.use(morgan('dev'))
+morgan.token('req-body', (req, res) => JSON.stringify(req.body))
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let people = [
     {
